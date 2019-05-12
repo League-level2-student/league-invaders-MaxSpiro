0db863e2d2269c4fb442faf031ecbf7dece6dd31
League Student <league@iMac-25.attlocal.net>
2019-05-12 15:56:03 -0700

    started invaders
    :sunglasses: :+1:

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,10 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	JFrame frame = new JFrame();
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+public static void main(String[] args) {
+	
+}
 }
